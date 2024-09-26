702a27631cb4906229bbab81666751a128980c48
root <root@Pequod>
2024-09-25 23:08:34 -0500

    Creating pretty tasks with cute form

--- src/App.js
+++ src/App.js
@@ -2,6 +2,13 @@ import React, { useState, useRef, useEffect } from "react";
 import Task from "./components/Task.js"
 import CreateTaskComponent from "./components/CreateTaskComponent.js";
 
+const centeredOnPage = {
+    justifyContent: "center",
+    height: "100vh",
+    alignItems: "center",
+    display: "flex",
+};
+
 const POMODORO_LENGTH_MS = 25 * 60 * 1000;
 
 export default function App() {
@@ -14,12 +21,10 @@ export default function App() {
     const startPomodoro = task => {
         nextTimeSwitch.current = Date.now() + POMODORO_LENGTH_MS;
         setActiveTask(task);
-        setTaskState(ActiveTaskState.ACTIVE_TASK);
     };
 
     const startBreak = () => {
         nextTimeSwitch.current = Date.now() + BREAK_LENGTH;
-        setTaskState(ActiveTaskState.ON_BREAK);
     };
 
     const finishActiveTask = () => {
@@ -28,37 +33,26 @@ export default function App() {
         startBreak(intervalId.current);
     };
 
-    const addTask = task => {
-        if (activeTask) {
-            setInactiveTasks([...inactiveTasks, task]);
-        } else {
-        }
-    }
+    // useEffect(() => {
+    //     if (activeTask == null) return;
+    //     intervalId.current = setInterval(() => {
+    //         if (timestamp.current == null) return;
+    //         if ((Date.now() - timestamp.current) < POMODORO_LENGTH_MS) return;
+    //     }, 1001);
+    // }, [activeTask]);
 
-    useEffect(() => {
-        if (activeTask == null) return;
-        intervalId.current = setInterval(() => {
-            if (timestamp.current == null) return;
-            if ((Date.now() - timestamp.current) < POMODORO_LENGTH_MS) return;
-        }, 1001);
-    }, [activeTask]);
-
-    return  activeTask ? 
-        <><div className="container">
-            <h2>Active Tasks</h2>
-            {activeTask ?
-                <Task name={activeTask.name} description={activeTask.description} /> :
-                <div className="task">
-                    <p>Waiting for Active Task</p>
+    return activeTask ?
+        <div style={centeredOnPage}>
+            <div className="row w-75">
+                <div className="col">
+                    <div className="card text-bg-primary">
+                        <div className="card-body">
+                            <h2 className="card-title">Active Task</h2>
+                            <p style={{ fontSize: "1.5rem", fontWeight: "bold" }} className="card-text">{activeTask.name}</p>
+                            <p className="card-text">{activeTask.description}</p>
+                        </div>
+                    </div>
                 </div>
-            }
-        </div></> :  <CreateTaskComponent addTask={addTask} />;
-
-    // {/* <div className="section">
-    // <h1>Manage Tasks</h2>
-    // {inactiveTasks.map(task => <Task
-    // name={task.name}
-    // description={task.description}
-    // />)}
-    // </div> */}
+            </div>
+        </div> : <CreateTaskComponent addTask={startPomodoro} />;
 }
